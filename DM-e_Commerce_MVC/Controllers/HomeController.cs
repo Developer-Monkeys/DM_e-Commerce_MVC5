678850daebdea24194f443e9d3b58b2fc941df25
Alperen <alpyurtseven99@gmail.com>
2020-06-29 23:16:38 +0300

    ss

--- DM-e_Commerce_MVC/Controllers/HomeController.cs
+++ DM-e_Commerce_MVC/Controllers/HomeController.cs
@@ -9,10 +9,6 @@ namespace DM_e_Commerce_MVC.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-
-
-        //Code Review Test
-
         public ActionResult Index()
         {
             return View();
